eacbb2d5590bc1e1c88e4ed9d840e856b1ae8fdd
ng3m8ruz <mancunianmerchant816@gmail.com>
2022-06-29 22:45:29 +0700

    Initial commit

---
+++ test/aws-serverless-microservices-by-mumet.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsServerlessMicroservicesByMumet from '../lib/aws-serverless-microservices-by-mumet-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-serverless-microservices-by-mumet-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsServerlessMicroservicesByMumet.AwsServerlessMicroservicesByMumetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
